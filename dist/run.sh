34af2ff1b04e20107fab48eaecbba2316d5fe8ac
Erlend Mongstad <eeerlend@gmail.com>
2019-05-07 13:21:28 +0200

    style: shellcheck compatible

--- dist/run.sh
+++ dist/run.sh
@@ -7,7 +7,8 @@ function symlink_dotfile {
   local chmod=$2
   local num_links=0
   local path_to_dotfiles="$HOME"/Library/Mobile\ Documents/com~apple~CloudDocs/.dotfiles
-  local path_to_file=$(dirname $file)
+  local path_to_file
+  path_to_file=$(dirname $file)
 
   # Create dirs locally if neccesary
   if [ ! -d "$HOME"/"$path_to_file" ]; then
@@ -22,13 +23,13 @@ function symlink_dotfile {
   
   # Detect if the file is a hard link
   if [ -f "$HOME"/"$file" ]; then
-    num_links=$(stat -f "%l" $HOME/$file)
+    num_links=$(stat -f "%l" "$HOME/$file")
   fi
 
   # If the file doesn't exist as hard link in your system, we'll create a hard link to icloud
   if [ "$num_links" != "2" ]; then
 
-    report_from_package "~/$file doesn't exist as hard link locally. Will create a hard link to iCloud"
+    report_from_package "\~/$file doesn't exist as hard link locally. Will create a hard link to iCloud"
 
     if [ -f "$HOME"/"$file" ]; then
       report_from_package "Removing static file $file"
@@ -46,16 +47,16 @@ function symlink_dotfile {
       unlink "$HOME"/"$file"
     fi
 
-    report_from_package "~/$file is already hard linked to iCloud"
+    report_from_package "\~/$file is already hard linked to iCloud"
   fi
   
   if [ -n "$chmod" ] && [ "$chmod" -eq "$chmod" ] 2>/dev/null; then
     if [ -L "$HOME"/"$file" ]; then
       report_from_package " chmod -h $chmod $HOME/$file"
-      chmod $chmod "$HOME"/"$file"
+      chmod "$chmod" "$HOME"/"$file"
     elif [ -f "$HOME"/"$file" ]; then
       report_from_package " chmod $chmod $HOME/$file"
-      chmod $chmod "$HOME"/"$file"
+      chmod "$chmod" "$HOME"/"$file"
     fi
   fi
 }
@@ -73,6 +74,8 @@ if [ ! -L .dotfiles-in-icloud ]; then
   ln -s "$HOME"/Library/Mobile\ Documents/com~apple~CloudDocs/.dotfiles .dotfiles-in-icloud
 fi
 
+# todo: check if array is declared up front!
+# shellcheck disable=SC2154
 if [ ${#macup_dotfiles_icloud[@]} -eq 0 ]; then
   report_from_package "No dotfiles to install. Addd files to the \$macup_dotfiles_icloud array" "yellow"
 fi
@@ -81,5 +84,5 @@ for ((i=0; i<${#macup_dotfiles_icloud[@]}; ++i)); do
   file="$(echo "${macup_dotfiles_icloud[i]}" | cut -d':' -f1)"
   chmod="$(echo "${macup_dotfiles_icloud[i]}" | cut -d':' -f2)"
 
-  symlink_dotfile $file $chmod
+  symlink_dotfile "$file" "$chmod"
 done
